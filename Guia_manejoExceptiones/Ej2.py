aa9a1668b0ab6abf54c4ab19f0248597ff27d4b4
JulianRosas <julymaxrosas@gmail.com>
2022-05-26 08:12:22 -0300

    ej2

---
+++ Guia_manejoExceptiones/Ej2.py
@@ -0,0 +1,9 @@
+try:
+    num1 = int(input("ingrese un numero: "))
+    num2 = int(input("ingrese otro  numero: "))
+
+    div = num1 / num2
+    print("la division es: ",div)
+except ZeroDivisionError:  
+    print("no se puede dividir por cero")
+    pass
